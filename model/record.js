7ccdbae1f04eb7152d7702f90c38941c11fc4ce2
fahri özmen <fahri@veriyaz.com.tr>
2020-07-19 13:25:51 +0300

    feat: Added search method to Record model.

--- model/record.js
+++ model/record.js
@@ -1,10 +1,56 @@
 const mongoose = require('mongoose');
 
-const Record = new mongoose.Schema({
+const schema = new mongoose.Schema({
   key: String,
   value: String,
   createdAt: Date,
   counts: Array
 });
 
-module.exports = mongoose.model('Record', Record);
+class Record {
+  static searchAsync({ startDate, endDate, minCount, maxCount }) {
+    return this.aggregate([
+      {
+        // Filter by dates.
+        $match: {
+          createdAt: {
+            $gte: new Date(startDate),
+            $lte: new Date(endDate)
+          }
+        }
+      },
+      {
+        $unwind: {
+          path: '$counts'
+        }
+      },
+      {
+        $group: {
+          _id: { key: '$key', createdAt: '$createdAt' },
+          key: { $first: '$key' },
+          createdAt: { $first: 'createdAt' },
+          totalCount: {
+            $sum: '$counts'
+          }
+        }
+      },
+      {
+        $match: {
+          totalCount: {
+            $gte: minCount,
+            $lte: maxCount
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      }
+    ]).exec();
+  }
+}
+
+schema.loadClass(Record);
+
+module.exports = mongoose.model('Record', schema);
--- routes/records.js
+++ routes/records.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const HttpStatus = require('http-status-codes');
 const Record = require('../model/record');
 
-router.post('/search', function (req, res) {
-  res.status(HttpStatus.OK).send(req.body);
+router.post('/search', async function (req, res) {
+  try {
+    let records = await Record.searchAsync(req.body);
+    res.status(HttpStatus.OK).send({ code: 0, msg: 'Success', records });
+  } catch (err) {
+    res.status(HttpStatus.BAD_REQUEST).send({
+      code: HttpStatus.BAD_REQUEST,
+      msg: `Search cannot be performed: Error: ${err.message}`
+    });
+  }
 });
 
 module.exports = router;
